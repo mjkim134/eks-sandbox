49bdc676a4d11471e4807c4e8cfc27b24fd97d50
mjkim134 <mjkim134@naver.com>
2026-06-08 15:39:46 +0900

    change backend app

--- app/request-service/src/main/java/click/mjkim/request/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package click.mjkim.request.repository;
-
-import click.mjkim.request.entity.RequestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestRepository extends JpaRepository<RequestEntity, Long> {
-}
