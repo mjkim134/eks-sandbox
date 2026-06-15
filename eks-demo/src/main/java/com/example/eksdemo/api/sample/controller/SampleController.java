64586b286db3002bb0291d7e1670bf56306ae1b4
mjkim134 <mjkim134@naver.com>
2026-06-15 16:13:22 +0900

    refactoring java proj

--- eks-demo/src/main/java/com/example/eksdemo/api/sample/controller/SampleController.java
+++ eks-demo/src/main/java/com/example/eksdemo/api/sample/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.example.eksdemo.api.sample.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,22 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/sample")
 public class SampleController {
-
-    @Value("${sample.id}")
-    private String sampleId;
-
-    @Value("${sample.password}")
-    private String samplePassword;
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
     @GetMapping("/hello")
     @ResponseStatus(HttpStatus.OK)
     public String getHello() {
+        logger.info("Call Get Method: Hello EKS");
         return "<h1>Hello EKS</h1>";
     }
-
-    @GetMapping("/testSecrets")
-        @ResponseStatus(HttpStatus.OK)
-        public String getSecrets() {
-            return "ID: " + sampleId + " / Password: " + samplePassword;
-        }
 }
