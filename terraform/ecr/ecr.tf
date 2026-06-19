79260c7ebb8cefba6f1479c89372014edc600dee
mjkim134 <mjkim134@naver.com>
2026-06-19 19:04:49 +0900

    delete same policy for ecr

--- terraform/ecr/ecr.tf
+++ terraform/ecr/ecr.tf
@@ -5,28 +5,3 @@ resource "aws_ecr_repository" "eks-demo" {
   }
   image_tag_mutability = "IMMUTABLE"
 }
-
-resource "aws_ecr_repository_policy" "eks-demo" {
-  repository = aws_ecr_repository.eks-demo.name
-  policy = jsonencode({
-    "Version" : "2008-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : [
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-          ]
-        },
-        "Action" : [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:DescribeRepositories",
-          "ecr:GetRepositoryPolicy",
-          "ecr:ListImages"
-        ]
-      }
-    ]
-  })
-}
--- terraform/ecr/github-actions-oidc.tf
+++ terraform/ecr/github-actions-oidc.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_openid_connect_provider" "github-actions-oidc" {
-  url = "https://token.actions.githubusercontent.com"
+  resource "aws_iam_openid_connect_provider" "github-actions-oidc" {
+    url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
+    client_id_list = [
+      "sts.amazonaws.com"
+    ]
 
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
-}
+    thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+  }
