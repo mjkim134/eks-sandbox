b6be57773168906a227d832a6c08c3cb1cb290e9
mjkim134 <mjkim134@naver.com>
2026-06-19 18:59:30 +0900

    add karpenter node iam role

--- terraform/modules/kimpala_eks/karpenter.tf
+++ terraform/modules/kimpala_eks/karpenter.tf
@@ -82,3 +82,39 @@ resource "aws_iam_role_policy_attachment" "karpenter_controller" {
   policy_arn = aws_iam_policy.karpenter_controller.arn
   role       = aws_iam_role.karpenter_controller.name
 }
+
+resource "aws_iam_role" "karpenter_node" {
+  name = "eks-${var.cluster_name}-karpenter-node"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_node_worker_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.karpenter_node.name
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_node_ecr_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.karpenter_node.name
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_node_ssm_policy" {
+  role       = aws_iam_role.karpenter_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "karpenter_node" {
+  name = "karpenter-node-instance-profile"
+  role = aws_iam_role.karpenter_node.name
+}
