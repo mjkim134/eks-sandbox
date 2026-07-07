3af568e35efdae5d2a9a6f75a49b7d8377f18007
mjkim134 <mjkim134@naver.com>
2026-07-07 21:20:06 +0900

    chore: update iam role name

--- terraform/modules/eks/karpenter.tf
+++ terraform/modules/eks/karpenter.tf
@@ -1,6 +1,6 @@
 ## karpenter controller
 resource "aws_iam_role" "karpenter_controller" {
-  name = "eks-${var.cluster_name}-karpenter-controller"
+  name = "${var.cluster_name}-karpenter-controller"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -22,7 +22,7 @@ resource "aws_iam_role" "karpenter_controller" {
 }
 
 resource "aws_iam_policy" "karpenter_controller" {
-  name = "eks-${var.cluster_name}-karpenter-controller-policy"
+  name = "${var.cluster_name}-karpenter-controller-policy"
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -84,7 +84,7 @@ resource "aws_iam_role_policy_attachment" "karpenter_controller" {
 }
 
 resource "aws_iam_role" "karpenter_node" {
-  name = "eks-${var.cluster_name}-karpenter-node"
+  name = "${var.cluster_name}-karpenter-node"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
