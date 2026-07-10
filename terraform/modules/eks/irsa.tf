d564fb80a81270d35c22e337912476e7bbf839e4
mjkim134 <mjkim134@naver.com>
2026-07-10 17:30:36 +0900

    feat: add external-secrets IRSA

--- terraform/modules/eks/irsa.tf
+++ terraform/modules/eks/irsa.tf
@@ -3,6 +3,10 @@ data "aws_route53_zone" "my_domain" {
   private_zone = false
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 module "load_balancer_controller_irsa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
 
@@ -52,6 +56,25 @@ module "external_dns_irsa" {
     this = {
       provider_arn               = module.eks.oidc_provider_arn
       namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+
+  tags = var.tags
+}
+
+module "external_secrets_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+
+  name = "${var.cluster_name}-external-secrets-role"
+  use_name_prefix = false
+
+  attach_external_secrets_policy        = true
+  external_secrets_ssm_parameter_arns   = ["arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/*"]
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-secrets:external-secrets"]
     }
   }
 
