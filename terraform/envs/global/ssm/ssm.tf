207750a9ab62380a6cf31e6025d60b454055ae89
mjkim134 <mjkim134@naver.com>
2026-07-14 18:20:01 +0900

    feat: add admin password for Grafana to SSM parameters

--- terraform/envs/global/ssm/ssm.tf
+++ terraform/envs/global/ssm/ssm.tf
@@ -17,3 +17,19 @@ resource "aws_ssm_parameter" "prod_slack_webhook" {
   key_id      = "alias/aws/ssm"
   value       = data.sops_file.secrets.data["prod_slack_webhook_url"]
 }
+
+resource "aws_ssm_parameter" "dev_grafana_admin_password" {
+  name        = "/eks-sandbox/dev/grafana/admin-password"
+  description = "Admin password for Grafana in Dev Environment"
+  type        = "SecureString"
+  key_id      = "alias/aws/ssm"
+  value       = data.sops_file.secrets.data["dev_admin_password"]
+}
+
+resource "aws_ssm_parameter" "prod_grafana_admin_password" {
+  name        = "/eks-sandbox/prod/grafana/admin-password"
+  description = "Admin password for Grafana in Prod Environment"
+  type        = "SecureString"
+  key_id      = "alias/aws/ssm"
+  value       = data.sops_file.secrets.data["prod_admin_password"]
+}
