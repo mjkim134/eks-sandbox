63c11fafd6f13fa886d7e7fe92669c477d1c5a05
mjkim134 <mjkim134@naver.com>
2026-08-05 20:01:20 +0900

    feat: eks oidc 의존성을 가진 irsa를 한 디렉토리로 묶기

---
+++ terraform/envs/dev/eks/irsa.tf
@@ -0,0 +1,134 @@
+module "keda_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+
+  name = "${var.cluster_name}-keda-role"
+  use_name_prefix = false
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "keda:keda-metrics-server",
+        "keda:keda-operator"
+      ]
+    }
+  }
+
+  policies = {
+    sqs_policy = aws_iam_policy.keda_policy.arn
+  }
+}
+
+resource "aws_iam_policy" "keda_policy" {
+  name = "${var.cluster_name}-keda-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = [
+          module.sqs.queue_arn,
+          module.sqs.dead_letter_queue_arn
+        ]
+      }
+    ]
+  })
+}
+
+module "eks_demo_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+
+  name = "${var.cluster_name}-eks-demo-role"
+  use_name_prefix = false
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["eks-demo:eks-demo"]
+    }
+  }
+
+  policies = {
+    sqs_policy = aws_iam_policy.eks_demo_policy.arn
+  }
+}
+
+resource "aws_iam_policy" "eks_demo_policy" {
+  name = "${var.cluster_name}-eks-demo-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:GetQueueUrl"
+        ]
+        Resource = [
+          module.sqs.queue_arn,
+          module.sqs.dead_letter_queue_arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "loki_role" {
+  name = "eks-sandbox-dev-loki-role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "${module.eks.oidc_provider_arn}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${module.eks.oidc_provider_arn}:aud" : "sts.amazonaws.com",
+            "${module.eks.oidc_provider_arn}:sub" : "system:serviceaccount:monitoring:loki"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "loki_policy" {
+  name        = "eks-sandbox-dev-loki-policy"
+  path        = "/"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": "arn:aws:s3:::eks-sandbox-dev-loki"
+        },
+        {
+            "Sid": "List",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": "arn:aws:s3:::eks-sandbox-dev-loki/*"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "loki_role_policy_attachment" {
+  policy_arn = aws_iam_policy.loki_policy.arn
+  role       = aws_iam_role.loki_role.name
+}
--- terraform/envs/dev/s3/main.tf
+++ terraform/envs/dev/s3/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "loki" {
   bucket = "eks-sandbox-dev-loki"
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
+resource "aws_s3_bucket_public_access_block" "loki" {
+  bucket = aws_s3_bucket.loki.id
 
   block_public_acls       = true
   block_public_policy     = true
