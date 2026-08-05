0b7a3b9a96aa616d707f3c0b269a75d04e93f8cd
mjkim134 <mjkim134@naver.com>
2026-08-05 20:01:48 +0900

    feat: eks oidc 의존성을 가진 irsa를 한 디렉토리로 묶기

--- terraform/envs/prod/apps/eks-demo/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-
-  config = {
-    bucket = "eks-sandbox-apne2-tfstate"
-    key    = "envs/prod/eks/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}
---
+++ terraform/envs/prod/eks/irsa.tf
@@ -0,0 +1,80 @@
+module "keda_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+
+  name = "${var.cluster_name}-keda-role"
+  use_name_prefix = false
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "keda:keda-metrics-server",
+        "keda:keda-operator"
+        ]
+    }
+  }
+
+  policies = {
+    sqs_policy = aws_iam_policy.keda_policy.arn
+  }
+}
+
+resource "aws_iam_policy" "keda_policy" {
+  name = "${var.cluster_name}-keda-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = [
+          module.sqs.queue_arn,
+          module.sqs.dead_letter_queue_arn
+        ]
+      }
+    ]
+  })
+}
+
+module "eks_demo_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+
+  name = "${var.cluster_name}-eks-demo-role"
+  use_name_prefix = false
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["eks-demo:eks-demo"]
+    }
+  }
+
+  policies = {
+    sqs_policy = aws_iam_policy.eks_demo_policy.arn
+  }
+}
+
+resource "aws_iam_policy" "eks_demo_policy" {
+  name = "${var.cluster_name}-eks-demo-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:GetQueueUrl"
+        ]
+        Resource = [
+          module.sqs.queue_arn,
+          module.sqs.dead_letter_queue_arn
+        ]
+      }
+    ]
+  })
+}
