8c52623739d51f566b2425aa6d23dea636fc3eed
mjkim134 <mjkim134@naver.com>
2026-08-05 18:52:16 +0900

    feat: loki storage를 위한 s3 구성

---
+++ terraform/envs/dev/s3/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.28"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "eks-sandbox-apne2-tfstate"
+    key          = "envs/global/s3/terraform.tfstate"
+    region       = "ap-northeast-2"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+
+  default_tags {
+    tags = {
+      Project   = "eks-sandbox"
+      Owner     = "minyx"
+      Terraform = "true"
+    }
+  }
+}
